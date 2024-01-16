26be54cf1c4d48785433772c78b2289123606ac2
Erion Ademi <117570706+Erion14@users.noreply.github.com>
2024-01-17 00:46:37 +0100

    Created Consumer Demo

--- Java/src/functional_programming/functions/consumers/ConsumerDemo.java
+++ Java/src/functional_programming/functions/consumers/ConsumerDemo.java
@@ -2,9 +2,12 @@ package functional_programming.functions.consumers;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import functional_programming.Products.DefaultProduct;
+import functional_programming.Products.Product;
 
 public class ConsumerDemo {
 	
@@ -18,11 +21,31 @@ public class ConsumerDemo {
         ));
 		
 		
-		
+		increasePriceForProductList(products, 10);
         for (DefaultProduct product : products) {
             System.out.println(product);
         }
+        
+        System.out.println("---BiConsumer Demo---");
+        Map<Integer, DefaultProduct> idProducMap = new HashMap<>();
+        idProducMap.put(1, new DefaultProduct(1, "Product 1", "Category 1", 99.99));
+        idProducMap.put(2, new DefaultProduct(2, "Product 2", "Category 2", 149.99));
+        idProducMap.put(3, new DefaultProduct(3, "Product 3", "Category 3", 39.99));
+        
+        increasePriceForProductMap(idProducMap, 20);
+        for (DefaultProduct defaultProduct : idProducMap.values()) {
+        	System.out.println(defaultProduct);
+			
+		}
+
 
 }
+	public static void increasePriceForProductList(List<? extends DefaultProduct> products, double priceToIncrease) {
+		products.iterator().forEachRemaining(product -> product.setPrice(product.getPrice()+ priceToIncrease));
+	}
+	public static void increasePriceForProductMap(Map<Integer, ? extends DefaultProduct> 
+																	idProductMap, double priceToIncrease) {
+		idProductMap.forEach((id, product) -> product.setPrice(product.getPrice()+priceToIncrease));
+	}
 }
 
