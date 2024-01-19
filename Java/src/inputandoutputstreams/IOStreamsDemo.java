e7eb94902e2ad42810b8eb75718a28e244dfb1c3
Erion Ademi <117570706+Erion14@users.noreply.github.com>
2024-01-19 17:36:30 +0100

    Created Input Output Streams Demo

--- Java/src/inputandoutputstreams/IOStreamsDemo.java
+++ Java/src/inputandoutputstreams/IOStreamsDemo.java
@@ -1,5 +1,41 @@
 package inputandoutputstreams;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class IOStreamsDemo {
 
+		public static void main(String[] args) throws IOException {
+			String filepath = "testDirectory" + File.separator + "demo.txt";
+			String textToWrite = "Some text example " + System.lineSeparator() + "with Line separator and cyrylic "
+					+ "characters: Erioni bossi " + System.lineSeparator();
+			
+			writeFileToPathFileOutputStream(filepath, textToWrite);
+			
+			
+			
+		}
+		private static void writeFileToPathFileOutputStream(String path, String textToWrite) 
+				throws FileNotFoundException, IOException{
+					FileOutputStream fos = null;
+				try {
+					fos = new FileOutputStream(path);
+					byte[] bytes = textToWrite.getBytes();
+					fos.write(bytes);
+				} finally {
+					if (fos != null) {
+						fos.close();
+					}
+				}
+				}
+		private static void writeFileToPathFileOutputStreamWithBuffer(String path, String textToWrite)
+				throws FileNotFoundException, IOException{
+			try(var fbos = new BufferedOutputStream(new FileOutputStream(path))){
+				fbos.write(textToWrite.getBytes());
+			}
+		}
+		
 }
