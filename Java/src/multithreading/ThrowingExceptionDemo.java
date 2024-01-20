89778f23dda4b7ca60fa28271215fad10604b027
Erion Ademi <117570706+Erion14@users.noreply.github.com>
2024-01-20 15:39:55 +0100

    Thread Yield, Group, Daemon Demo

---
+++ Java/src/multithreading/ThrowingExceptionDemo.java
@@ -0,0 +1,18 @@
+package multithreading;
+
+public class ThrowingExceptionDemo {
+	
+	public static void main(String[] args) {
+		var t0 = new Thread(() -> {
+				Thread.yield();
+				System.out.println(Thread.currentThread().getName());
+		});
+	
+		var t1 = new Thread(() -> System.out.println(Thread.currentThread().getName()));
+		
+		
+		t0.start();
+		t1.start();
+	}
+
+}
---
+++ Java/src/multithreading/YieldDemo.java
@@ -0,0 +1,5 @@
+package multithreading;
+
+public class YieldDemo {
+
+}
