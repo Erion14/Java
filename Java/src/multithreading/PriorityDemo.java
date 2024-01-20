ab4e08bd72f6b1060500e38e1cf8110072d08b4a
Erion Ademi <117570706+Erion14@users.noreply.github.com>
2024-01-20 15:35:20 +0100

    Thread Priority Demo

---
+++ Java/src/multithreading/PriorityDemo.java
@@ -0,0 +1,32 @@
+package multithreading;
+
+public class PriorityDemo {
+
+	public static void main(String[] args) {
+		var t0 = new Thread(PriorityDemo::execute);
+		var t1 = new Thread(PriorityDemo::execute);
+		var t2 = new Thread(PriorityDemo::execute);
+		var t3 = new Thread(PriorityDemo::execute);
+		var t4 = new Thread(PriorityDemo::execute);
+		var t5 = new Thread(PriorityDemo::execute);
+
+		t0.setPriority(1);
+		t1.setPriority(8);
+		t2.setPriority(10);
+		t3.setPriority(10);
+		t4.setPriority(5);
+		t5.setPriority(3);
+
+		t0.start();
+		t1.start();
+		t2.start();
+		t3.start();
+		t4.start();
+		t5.start();
+
+	}
+
+	public static void execute() {
+		System.out.println(Thread.currentThread().getName());
+	}
+}
