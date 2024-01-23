dd435ff7272a6c68c261fa3dba18c6f0b5924d51
Erion Ademi <117570706+Erion14@users.noreply.github.com>
2024-01-23 22:00:19 +0100

    Completable Future Demos

---
+++ Java/src/multithreading/completablefuture/Demo1.java
@@ -0,0 +1,28 @@
+package multithreading.completablefuture;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class Demo1 {
+
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
+		CompletableFuture.runAsync(() -> {
+			System.out.println("Thread name " + Thread.currentThread().getName());
+			System.out.println("http call goes here...");
+		});
+
+		TimeUnit.SECONDS.sleep(2);
+
+		ExecutorService es = Executors.newCachedThreadPool();
+		CompletableFuture.runAsync(() -> {
+			System.out.println("Thread name " + Thread.currentThread().getName());
+			System.out.println("http call goes here...");
+
+		}, es);
+		es.shutdown();
+	}
+
+}
