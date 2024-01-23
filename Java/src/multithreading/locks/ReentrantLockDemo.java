b43824e5164e865aa37236cee7f457c3c15e9f22
Erion Ademi <117570706+Erion14@users.noreply.github.com>
2024-01-23 22:29:02 +0100

    ReentrantLock Demo

---
+++ Java/src/multithreading/locks/ReentrantLockDemo.java
@@ -0,0 +1,46 @@
+package multithreading.locks;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.stream.IntStream;
+
+public class ReentrantLockDemo {
+
+	private static int counter;
+
+	private Lock lock = new ReentrantLock();
+	
+	public static void main(String[] args) throws InterruptedException{
+		var thisInstance = new ReentrantLockDemo();
+		var es = Executors.newFixedThreadPool(4);
+		IntStream.range(0, 10000).forEach((i -> es.execute(thisInstance::incrementWithLock)));
+		terminateExecutorService(es);
+		
+		System.out.println(counter);
+	}
+
+	public void incrementWithLock() {
+		try {
+			lock.lock();
+			counter++;
+		} finally {
+			lock.unlock();
+			// TODO: handle finally clause
+		}
+	}
+	
+	public void increment() {
+		synchronized(this) {
+			counter++;
+		}
+	}
+	
+	private static void terminateExecutorService(ExecutorService es) throws InterruptedException{
+		es.shutdown();
+		es.awaitTermination(3, TimeUnit.SECONDS);
+		es.shutdownNow();
+	}
+}
