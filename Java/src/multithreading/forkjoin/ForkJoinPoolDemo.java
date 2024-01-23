a79e21659ad44e062bbb1a7bcb054e253f55cfb9
Erion Ademi <117570706+Erion14@users.noreply.github.com>
2024-01-23 13:10:03 +0100

    Fork Join Examples

---
+++ Java/src/multithreading/forkjoin/ForkJoinPoolDemo.java
@@ -0,0 +1,17 @@
+package multithreading.forkjoin;
+
+import java.util.concurrent.ForkJoinPool;
+
+public class ForkJoinPoolDemo {
+
+	public static void main(String[] args) {
+		ForkJoinPool commonPool = ForkJoinPool.commonPool();
+		
+		ForkJoinPool forkJoinPool = new ForkJoinPool(4);
+		
+		ForkJoinPool forkJoinPool2 = new ForkJoinPool();
+		
+		forkJoinPool2.invoke(new DefaultRecursiveAction(40));
+
+	}
+}
