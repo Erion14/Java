b10507ff49a2a34683127ebfcce83ecd023b8534
Erion Ademi <117570706+Erion14@users.noreply.github.com>
2024-02-08 22:33:52 +0100

    Annotation Program

---
+++ Java/src/annotation/Todo.java
@@ -0,0 +1,25 @@
+package annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@interface Todo {
+	public enum Priority {
+		LOW, MEDIUM, HIGH
+	}
+
+	public enum Status {
+		STARTED, NOT_STARTED
+	}
+
+	String author();
+
+	Priority priority() default Priority.LOW;
+
+	Status status() default Status.NOT_STARTED;
+
+}
