fbfd0641de31dc6e544495985351d890ccd99a11
Erion Ademi <117570706+Erion14@users.noreply.github.com>
2024-02-16 16:04:29 +0100

    Problem with optional

---
+++ Java/src/optional/ProblemDemo.java
@@ -0,0 +1,43 @@
+package optional;
+
+public class ProblemDemo {
+
+	public static void main(String[] args) {
+		var computer = new Computer();
+		var usbDrive = computer.getUsbDrive();
+
+		if (usbDrive != null) {
+			var writeProtectSwitch = usbDrive.getWriteProtectSwitch();
+			if (writeProtectSwitch != null) {
+				// some code for write protect switch
+			}
+		} else {
+
+		}
+
+	}
+
+	private static class Computer {
+
+		private UsbDrive usbDrive;
+
+		public UsbDrive getUsbDrive() {
+			return this.usbDrive;
+		}
+
+	}
+
+	private static class UsbDrive {
+
+		private WriteProtectSwitch writeProtectSwitch;
+
+		public WriteProtectSwitch getWriteProtectSwitch() {
+			return writeProtectSwitch;
+		}
+
+	}
+
+	private static class WriteProtectSwitch {
+
+	}
+}
