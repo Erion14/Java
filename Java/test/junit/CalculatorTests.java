db83de2dcc304afc262186e470598216b86cf46b
Erion Ademi <117570706+Erion14@users.noreply.github.com>
2024-02-26 12:45:23 +0100

    First Unit Test implemented

---
+++ Java/test/junit/CalculatorTests.java
@@ -0,0 +1,17 @@
+package junit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalculatorTests {
+
+	@Test
+	void test() {
+		var calculator = new Calculator();
+		int actual = calculator.add(2, 3);
+		
+		assertEquals(5, actual);
+	}
+
+}
